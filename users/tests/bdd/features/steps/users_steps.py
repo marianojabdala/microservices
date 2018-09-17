174c6a92b94edb119586a3186094f4709b426fb3
Mariano <marianoabdala@gmail.com>
2018-09-17 16:02:44 -0300

    Improve code quality
    Remove build-in asserts on tests.
    Add categories to bdd tests.

--- users/tests/bdd/features/steps/users_steps.py
+++ users/tests/bdd/features/steps/users_steps.py
@@ -1,13 +1,13 @@
 from behave import given, when, then
 from json import dumps as json_dump, loads as json_load
+import sure
 
 @given("The user set the {username} and {password}")
 def step_impl(context, username, password):
     context.username = username
     context.password = password
-
-    assert username is not None
-    assert password is not None
+    (username).should_not.be.none
+    (password).should_not.be.none
 
 @when("User user post to the /users endpoint")
 def step_impl_1(context):
@@ -17,19 +17,21 @@ def step_impl_1(context):
         "password": context.password
     }
 
-    res = context.client().post('/users', data = json_dump(user),
-                             headers={"Content-Type": "application/json"})
+    res = context.client().post('/users', data=json_dump(user),
+                                headers={"Content-Type": "application/json"})
 
-    context.status_code = res.status_code;
+    context.status_code = res.status_code
     context.data = res.data
 
 @then("The response is 201 and the user is created")
 def step_impl_2(context):
-    assert context.status_code == 201
+
+    context.status_code.should.be.equal(201)
 
     users = json_load(context.data)["users"]
 
-    assert users["_id"] is not None
-    assert users["name"] == context.username
-    assert users["admin"] == False
-    assert users["uri"] == "/users/1"
+    users.should.be.a('dict')
+    users.should.have.key("_id").being.equal(1)
+    users.should.have.key("name").being.equal(context.username)
+    users.should.have.key("admin").shouldnt.be.ok
+    users.should.have.key("uri").being.equal("/users/1")
