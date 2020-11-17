323d1fba3577ded5b4c543c81b98ee965efb58c7
Mariano Abdala <marianoabdala@gmail.com>
2020-11-16 22:06:45 -0300

    Fix linting
    Fix security concerns

--- users/tests/config.py
+++ users/tests/config.py
@@ -8,10 +8,9 @@ from os import environ, getuid, getgid
 
 workers = cpu_count() + 1
 threads = 1
-daemon = environ.get("DAEMON")
 certfile="microservice-example.org+4.pem"
 keyfile="microservice-example.org+4-key.pem"
 user=getuid()
 group=getgid()
-bind = f"{gethostbyname(gethostname())}:5000"
+bind = f"0.0.0.0:5000"
 access_log_format='%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s %(D)sµs"'
